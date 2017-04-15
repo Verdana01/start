58a549e887cc3861aa38b40e6e74f2980d09b670
Yassir <tahomafs96@gmail.com>
2017-04-15 17:55:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,260 @@
+<?php require('Header.php'); ?>
+<div id="preloader">
+  <div id="status"> <img src="img/preloader.gif" height="64" width="64" alt=""> </div>
+</div>
+<!-- Navigation
+    ==========================================-->
+
+
+<!-- Header -->
+<header class="text-center" name="home">
+  <div class="intro-text">
+    <h1 class="wow fadeInDown">We are <strong><span class="color">UltraViolet</span></strong></h1>
+    <p class="wow fadeInDown">Best places and events around the world</p>
+    <a href="#works-section" class="btn btn-default btn-lg page-scroll wow fadeInUp" data-wow-delay="200ms">Read More</a> </div>
+</header>
+
+<!-- Services Section -->
+<div id="services-section" class="text-center">
+  <div class="container">
+    <div class="section-title wow fadeInDown">
+      <h2>Overall Best <strong>Countries</strong>Ranking</h2>
+      <hr>
+      <div class="clearfix"></div>
+      <p>Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus. Eleifend pellentesque natoque faucibus magna ut etiam.</p>
+    </div>
+    <div class="space"></div>
+    <div class="row">
+      <div class="col-sm-6 service wow fadeInUp" data-wow-delay="200ms">
+        <p class="paragraph">Switzerland is the best country in the world, according to a new study from U.S. News & World Report.</p>
+        <p class="paragraph">The media organisation — notable for its influential US university rankings — evaluated 80 countries across a range of criteria, from economic influence to citizenship and quality of life. </p>
+        <p class="paragraph">By surveying more than 21,000 business leaders, informed elites, and general citizens, the aim of the "2017 Best Countries" report is to discover how nations are perceived on a global scale.</p><br>
+        <a href="https://www.usnews.com/news/best-countries"  class="btn btn-default btn-lg Ctr-btn" target="_blank" >See the full ranking</a>
+      </div>
+      <div class="col-sm-6 service wow fadeInUp" data-wow-delay="400ms"> 
+        <img src="img/countries/Sweden.jpg">
+        <h5><strong>Sweden is the highest ranking Nordic nation on the list.</strong></h5>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Portfolio Section -->
+<div id="works-section" class="text-center">
+  <div class="container"> <!-- Container -->
+    <div class="section-title wow fadeInDown">
+      <h2><strong>Events</strong> and <strong>Festivals</strong></h2>
+      <hr>
+      <div class="clearfix"></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamcommodo nibh ante facilisis.</p>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        <li>
+          <ol class="type">
+            <li><a href="#" data-filter="*" class="active">All</a></li>
+            <li><a href="#" data-filter=".web">Musical festivals</a></li>
+            <li><a href="#" data-filter=".app">Film festivals</a></li>
+            <li><a href="#" data-filter=".branding">Cultural</a></li>
+          </ol>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="200ms">
+            <div class="hover-bg"> <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>UltraMusic</h4>
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/01.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="400ms">
+            <div class="hover-bg"> <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>Cannes</h4>
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/02.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="600ms">
+            <div class="hover-bg"> <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>Tomorrowland</h4>
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/03.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="800ms">
+            <div class="hover-bg"> <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>Exit</h4>
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/04.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="1000ms">
+            <div class="hover-bg"> <a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>Marrakech International <br> Festival of film</h4>
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/05.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 branding">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="1200ms">
+            <div class="hover-bg"> <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>Holi Festival</h4>
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/06.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 branding app">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="1400ms">
+            <div class="hover-bg"> <a href="#portfolioModal7" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                App Development, Branding
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/07.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item wow fadeInUp" data-wow-delay="1600ms">
+            <div class="hover-bg"> <a href="#portfolioModal8" class="portfolio-link" data-toggle="modal">
+              <div class="hover-text">
+                <h4>Mawazine</h4>
+                <div class="clearfix"></div>
+                <i class="fa fa-plus"></i> </div>
+              <img src="img/portfolio/08.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- About Section -->
+<div id="about-section">
+  <div class="container">
+    <div class="section-title text-center wow fadeInDown">
+      <h2><strong>About</strong> us</h2>
+      <hr>
+      <div class="clearfix"></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamcommodo nibh ante facilisis.</p>
+    </div>
+    <!--style="height: 280px; border-radius: 3px;"-->
+    <div class="row">
+      <div class="col-md-6 wow fadeInLeft"> <img src="img/about.png"  class="img-responsive"> </div>
+      <div class="col-md-6 wow fadeInRight">
+          <h4>Who We Are</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh ante facilisis bibendum.</p>
+          <div class="space"></div>
+          <h4>What We Do</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at.</p>
+          <div class="space"></div><div class="list-style">
+            <div class="row">
+              <div class="col-lg-6 col-sm-6 col-xs-12">
+                <ul>
+                  <li>Lorem ipsum dolor</li>
+                  <li>Consectetur adipiscing</li>
+                  <li>Duis sed dapibus leo</li>
+                  <li>Sed commodo nibh ante</li>
+                </ul>
+              </div>
+              <div class="col-lg-6 col-sm-6 col-xs-12">
+                <ul>
+                  <li>Lorem ipsum dolor</li>
+                  <li>Consectetur adipiscing</li>
+                  <li>Duis sed dapibus leo</li>
+                  <li>Sed commodo nibh ante</li>
+                </ul>
+              </div>
+            </div>
+          </div>     
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Contact Section -->
+<div id="contact-section" class="text-center">
+  <div class="container">
+    <div class="section-title wow fadeInDown">
+      <h2><strong>Contact</strong> us</h2>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamcommodo nibh ante facilisis.</p>
+    </div>
+    <div class="col-md-8 col-md-offset-2 wow fadeInUp" data-wow-delay="200ms">
+      <div class="col-md-4"> <i class="fa fa-map-marker fa-2x"></i>
+        <p>Boulevard Allal El Fassi<br>
+          Marrakech</p>
+      </div>
+      <div class="col-md-4"> <i class="fa fa-envelope-o fa-2x"></i>
+        <p>TahomaFS96@gmail.com</p>
+      </div>
+      <div class="col-md-4"> <i class="fa fa-phone fa-2x"></i>
+        <p>+212 673112498</p>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+    <div class="col-md-8 col-md-offset-2 wow fadeInUp" data-wow-delay="400ms">
+	<h3>Leave us a message</h3>
+      <form name="sentMessage" id="contactForm" action="mail/contact_me.php" method="post">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" name="name" maxlength="50" placeholder="Name" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" name="email" maxlength="50" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" name="message" maxlength="500" rows="4" placeholder="Message" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-default">Send Message</button>
+      </form>
+      <div class="social">
+        <ul>
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+          <li><a href="#"><i class="fa fa-github"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container">
+      <p>Copyright © Tempo. Design by <a href="http://www.templategarden.com" rel="nofollow">TemplateGarden</a></p>
+  </div>
+</div>
+
+<!-- Portfolio Modals --> 
+<!-- Use the modals below to showcase details about your portfolio projects! --> 
+<?php require('modals.php'); ?>
+
+<?php require('footer.php'); ?>
